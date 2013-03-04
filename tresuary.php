1809360d649740ce2606779622332623d6cffb4f
naomi <naomi@aktivix.org>
2013-03-04 16:46:35 +0000

    Extracted an sql query into DataRetrieval class. Plus formatting

--- tresuary.php
+++ tresuary.php
@@ -4,6 +4,8 @@ include('inc/inc.php');
 include_lcm('inc_acc');
 include_lcm('inc_filters');
 include_lcm('inc_obj_fu');
+include('inc/DataRetrieval.class.php');
+
 
 global $author_session;
 
@@ -19,16 +21,7 @@ $fu_zomg = new lcmFollowupInfoUI();
 $fu_zomg->data['type']='opening';
 $fu_zomg->printEdit();
 
-$q = 'select name_first, name_last, cl.id_client, amount, c.id_case 
-	from lcm_case as c 
-	left join lcm_case_client_org as cco on c.id_case = cco.id_case
-	left join lcm_client as cl on cl.id_client = cco.id_client
-	where c.amount > 0 and c.status="open"
-	order by cl.name_first, cl.name_last
-	';
-
-
-$result = lcm_query($q);
+$data = DataRetrieval::getIdAmountAndClientNameForAllOpenCasesOverZero();
 
 $headers = array();
 $headers[0]['title'] = 'Client';
@@ -40,8 +33,8 @@ $headers[5]['title'] = 'Signature';
 
 show_list_start($headers);
 $i = 0;
-while ($row = lcm_fetch_array($result))
-	{
+foreach ($data as $row)
+{
 	echo "<tr>\n";
 	echo "<td  width = '20%' class='tbl_cont_" . ($i % 2 ? "dark" : "light") . "'>";
 	echo "<a class='content_link' href='client_det.php?client=".$row['id_client']."'>" . get_person_name($row) . "</a>";
@@ -53,12 +46,12 @@ while ($row = lcm_fetch_array($result))
 	echo "<td width = '10%' class='tbl_cont_" . ($i % 2 ? "dark" : "light") . "'>";
 	echo '<select name="amount_'.$row['id_case'].'">';
 	for ($j=0;$j<=60;$j=$j+5)
-		{
+	{
 		$sel='';
 		if (clean_output($row['amount'])==$j)
 			$sel='selected';
 		echo '<option value="'.$j.'" '.$sel.'>'.$j.'</option>';
-		}
+	}
 	echo '</select>';
 	echo "</td>\n";
 
@@ -77,10 +70,10 @@ while ($row = lcm_fetch_array($result))
 		";
 	$notes = lcm_query($q);
 	while ($note = lcm_fetch_array($notes))
-		{
+	{
 		echo "<input type='checkbox' name='dismiss_" . $note['id_app']."'/>";
 		echo $note['description'].' <small><i>(by '.get_person_name($note).' on '.format_date($note['date_creation'],'date_short').')</i></small><br />';
-		}
+	}
 	echo "<input type='text' name='note_".$row['id_case']."' class='search_form_txt'/>";
 	echo "</td>\n";
 
@@ -90,7 +83,7 @@ while ($row = lcm_fetch_array($result))
 	
 	echo "</tr>\n";
 	$i++;
-	}
+}
 echo "</table>";
 echo "<p><button name='submit' value='submit' class='simple_form_btn'>Submit</button></p>";
 echo "</form>";
@@ -100,13 +93,13 @@ $result = lcm_query('select amount, count(id_case) as ttl from lcm_case where st
 
 echo "<table class='tbl_usr_dtl'><tr><td><b>Quantity</b></td><td><b>Amount</b></td></tr>";
 while ($row = lcm_fetch_array($result))
-	{
+{
 	echo "<tr><td>";
 	echo $row['ttl'];
 	echo "</td><td>";
 	echo $row['amount'];
 	echo "</td></tr>";
-	}
+}
 
 $row = lcm_fetch_array(lcm_query('select sum(amount) ttl from lcm_case where amount > 0'));
 echo "<tr><td><b>Total:</b></td><td>";

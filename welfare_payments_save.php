bfce1bd25fdda90846bb1152f6e64fa0c4ce868e
naomi <naomi@aktivix.org>
2013-04-17 19:38:24 +0100

    and more path issues

--- welfare_payments_save.php
+++ welfare_payments_save.php
@@ -28,9 +28,10 @@ for($i = 0; $i < sizeof($_POST['id_case']); $i ++) {
 // Deals with the fact that casemanager might be on the doc root
 // or might be in a directory below it.
 $dir = dirname($_SERVER['PHP_SELF']);
-if (strlen($dir > 1)) {
+if (strlen($dir) > 1) {
         header("Location: $dir/welfare_payments.php?tab=record", true, 303);
 } else {
         header('Location: /welfare_payments.php?tab=record', true, 303);
 }
+?>
 ~ 

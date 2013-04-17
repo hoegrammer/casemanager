94bce83fbc52fba993096782c502157922adf51a
naomi <naomi@aktivix.org>
2013-04-17 19:33:41 +0100

    Further bugfix for hard-coded path.

--- welfare_payments_save.php
+++ welfare_payments_save.php
@@ -25,4 +25,12 @@ for($i = 0; $i < sizeof($_POST['id_case']); $i ++) {
 	}
 }
 
-header('Location: '. dirname($_SERVER['PHP_SELF']) .'/welfare_payments.php?tab=record', true, 303);
+// Deals with the fact that casemanager might be on the doc root
+// or might be in a directory below it.
+$dir = dirname($_SERVER['PHP_SELF']);
+if (strlen($dir > 1)) {
+        header("Location: $dir/welfare_payments.php?tab=record", true, 303);
+} else {
+        header('Location: /welfare_payments.php?tab=record', true, 303);
+}
+~ 
